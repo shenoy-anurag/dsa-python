a63f012c9626c190ea0020e1ac6fac9200bc4d1f
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2025-01-30 01:01:01 -0500

    fix: correct implementation of karatsuba multiplication algorithm

--- dsa_python/algorithms/divide_n_conquer/karatsuba_multiplication.py
+++ dsa_python/algorithms/divide_n_conquer/karatsuba_multiplication.py
@@ -16,13 +16,15 @@ def karatsuba_mul(num1, num2):
     # print("n1: {}, n2: {}".format(n1, n2))
     # print(num1, num2)
     n = max(n1, n2)
-    if n1 <= 2 or n2 <= 2:
+    if n1 == 1 or n2 == 1:
         return num1 * num2
     if n % 2 == 0:
-        n_by_2 = int(math.floor(n/2))
+        # n_by_2 = int(math.floor(n/2))
+        n_by_2 = int(n//2)
     else:
         n += 1
-        n_by_2 = int(math.floor(n/2)) - 1
+        # n_by_2 = int(math.floor(n/2)) - 1
+        n_by_2 = int(n//2) - 1
     a, b = split_num(num1, n_by_2)
     c, d = split_num(num2, n_by_2)
     ac = karatsuba_mul(a, c)
@@ -31,9 +33,31 @@ def karatsuba_mul(num1, num2):
     return 10**(n) * ac + 10**(n_by_2)*ad_plus_bc + bd
 
 
+def karat(x,y):
+    if len(str(x)) == 1 or len(str(y)) == 1:
+        return x*y
+    else:
+        m = max(len(str(x)),len(str(y)))
+        m2 = m // 2
+
+        a = x // 10**(m2)
+        b = x % 10**(m2)
+        c = y // 10**(m2)
+        d = y % 10**(m2)
+
+        z0 = karat(b,d)
+        z1 = karat((a+b),(c+d))
+        z2 = karat(a,c)
+
+        return (z2 * 10**(2*m2)) + ((z1 - z2 - z0) * 10**(m2)) + (z0)
+
+
 if __name__ == '__main__':
-    x = 5678
-    y = 1234
+    # x = 5678
+    # y = 1234
+    
+    x = 3141592653589793238462643383279502884197169399375105820974944592
+    y = 2718281828459045235360287471352662497757247093699959574966967627
 
     # n_by_2 = len(str(x))//2
     # a, b = split_num(x, n_by_2)
@@ -42,4 +66,5 @@ if __name__ == '__main__':
     # print(a, b)
 
     print(karatsuba_mul(x, y))
+    print(karat(x, y))
     print(x * y)
